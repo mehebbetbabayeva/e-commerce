9251fe3f2672121605ada7b98e6e35aef54ff3ba
mehebbetbabayeva <mhbbtbabaeva97@gmail.com>
2023-03-25 13:26:03 +0400

    finish fix

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import Header from "./components/Header";
 import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home";
 import Shop from "./pages/Shop";
-import Blog from "./pages/Blog";
-import Contact from "./pages/Contact";
-import Search from "./pages/Search";
-import Login from "./pages/Login";
 import MobileMenu from "./pages/MobileMenu";
 function App() {
   return (
@@ -14,11 +10,14 @@ function App() {
     <MobileMenu/>
     <Routes>
     <Route path="/" element={<Home/>}/> 
+    <Route path="/women" element={<Home/>}/>
+    <Route path="/men" element={<Home/>}/>
     <Route path="/shop" element={<Shop/>}/>
-    <Route path="/blog" element={<Blog/>}/>
-    <Route path="/contact" element={<Contact/>}/>
-    <Route path="/search" element={<Search/>}/>
-    <Route path="/login" element={<Login/>}/>
+    <Route path="/blog" element={<Home/>}/>
+    <Route path="/contact" element={<Home/>}/>
+    <Route path="/search" element={<Home/>}/>
+    <Route path="/login" element={<Home/>}/>
+    <Route path="/pages" element={<Home/>}/>
     </Routes> 
     
     </>
